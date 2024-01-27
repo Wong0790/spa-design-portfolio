cb20e7eb7d061659da4420f6dd71ffd4bcc52a38
Anne <anneris.wong@gmail.com>
2024-01-26 21:32:12 -0500

    Update imports

--- src/components/Icons/index.js
+++
@@ -1,9 +0,0 @@
-export { default as AppsIcon } from "./AppsIcon.vue";
-export { default as CameraIcon } from "./CameraIcon.vue";
-export { default as GraphicDesignIcon } from "./GraphicDesignIcon.vue";
-export { default as IlustrationsIcon } from "./IlustrationsIcon.vue";
-export { default as MotionGraphicsIcon } from "./MotionGraphicsIcon.vue";
-export { default as PhotographyIcon } from "./PhotographyIcon.vue";
-export { default as UxUiIcon } from "./UxUiIcon.vue";
-export { default as ArrowRight } from "./ArrowRight.vue";
-export { default as ArrowLeft } from "./ArrowLeft.vue";
