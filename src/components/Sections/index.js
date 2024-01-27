bea0d1bcc9bc2f32c1624e8e773b90a7f4bd49d8
Anne <anneris.wong@gmail.com>
2024-01-26 21:51:47 -0500

    update

--- src/components/Sections/index.js
+++
@@ -1,4 +0,0 @@
-export { default as AboutSection } from "./AboutSection.vue";
-export { default as ConsultationSection } from "./ConsultationSection.vue";
-export { default as SkillsSection } from "./SkillsSection.vue";
-export { default as WorkSection } from "./WorkSection.vue";
