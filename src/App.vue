918965954845aed36fec853fad134b37f432f51a
Anne <anneris.wong@gmail.com>
2024-01-26 22:15:47 -0500

    Update sections import

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,9 @@
 <script setup lang="ts">
 import FreeConsultation from "@components/FreeConsultation.vue";
-import {
-  SkillsSection,
-  AboutSection,
-  WorkSection,
-  ConsultationSection,
-} from "@sections";
-/* import SkillsSection from "@sections/SkillsSection.vue";
+import SkillsSection from "@sections/SkillsSection.vue";
 import AboutSection from "@sections/AboutSection.vue";
 import WorkSection from "@sections/WorkSection.vue";
-import ConsultationSection from "@sections/ConsultationSection.vue"; */
+import ConsultationSection from "@sections/ConsultationSection.vue";
 </script>
 
 <template>
